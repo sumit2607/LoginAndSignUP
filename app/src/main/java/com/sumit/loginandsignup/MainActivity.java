41f4e00537d214b3c5486fda24aa47f9b1c288e1
Sumit Rai <40994984+sumit2607@users.noreply.github.com>
2021-07-02 18:36:27 +0530

    Initial commit

---
+++ app/src/main/java/com/sumit/loginandsignup/MainActivity.java
@@ -0,0 +1,18 @@
+package com.sumit.loginandsignup;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ImageSwitcher;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+    }
+}
